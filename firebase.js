009fc4e5b73039c07b25e482a643707f30c038a3
Bhaskar-Newase-Ext <62342525+Bhaskar-Newase-Ext@users.noreply.github.com>
2021-08-11 21:26:19 +0530

    auth and card functionality

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0X-8vRoteCZ6jBslIbVG09FtjBJO0ns8",
+    authDomain: "amzn-2-52c0b.firebaseapp.com",
+    projectId: "amzn-2-52c0b",
+    storageBucket: "amzn-2-52c0b.appspot.com",
+    messagingSenderId: "153928710660",
+    appId: "1:153928710660:web:667d7d96d0078a494e9351"
+  };
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -4,12 +4,13 @@ function ProductFeed({ products }) {
     
     return (
         <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-            {products.slice(0,4).map(({id, title, price, description, category, image}) => (
+            {products.slice(0,4).map(({id, title, price, rating, description, category, image, hasPrime}) => (
                 <Product 
                     key={id}
                     id={id}
                     title={title}
                     price={price}
+                    rating={rating}
                     description={description}
                     category={category}
                     image={image}
@@ -22,12 +23,13 @@ function ProductFeed({ products }) {
             />
 
             <div className="md:col-span-2">
-            {products.slice(4,5).map(({id, title, price, description, category, image}) => (
+            {products.slice(4,5).map(({id, title, price, rating, description, category, image, hasPrime}) => (
                 <Product 
                     key={id}
                     id={id}
                     title={title}
                     price={price}
+                    rating={rating}
                     description={description}
                     category={category}
                     image={image}
@@ -35,12 +37,13 @@ function ProductFeed({ products }) {
             ))}
             </div>
 
-            {products.slice(5,products.length).map(({id, title, price, description, category, image}) => (
+            {products.slice(5,products.length).map(({id, title, price, rating, description, category, image, hasPrime}) => (
                 <Product 
                     key={id}
                     id={id}
                     title={title}
                     price={price}
+                    rating={rating}
                     description={description}
                     category={category}
                     image={image}
