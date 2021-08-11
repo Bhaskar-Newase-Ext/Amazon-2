58ef3119a90527315694be3f4ac66e9cb2becdfd
Bhaskar-Newase-Ext <62342525+Bhaskar-Newase-Ext@users.noreply.github.com>
2021-08-11 23:51:54 +0530

    back to top btn and animation on product

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,49 @@
 function Footer() {
+
+    const scrollToTop = () => {
+        window.scrollTo(0, 0);
+    }
+
     return (
-        <div className="grid grid-flow-col grid-cols-4 bg-gray-800 text-white px-20 py-10">
-            <div className="px-10 leading-10">
-                <p className="font-bold">Get to Know Us</p>
-                <p className="text-sm leading-7 text-gray-300">About Us</p>
-                <p className="text-sm leading-7 text-gray-300">Careers</p>
-                <p className="text-sm leading-7 text-gray-300">Press Releases</p>
-                <p className="text-sm leading-7 text-gray-300">Amazon Cares</p>
-                <p className="text-sm leading-7 text-gray-300">Gift a Smile</p>
+        <div>
+            <div>
+                <button onClick={scrollToTop} className="mt-2 text-white py-4 text-sm w-full bg-gray-700 hover:bg-gray-600">Back to top</button>
             </div>
-            <div className="px-10 leading-10">
-                <p className="font-bold">Connect with Us</p>
-                <p className="text-sm leading-7 text-gray-300">Facebook</p>
-                <p className="text-sm leading-7 text-gray-300">Twitter</p>
-                <p className="text-sm leading-7 text-gray-300">Instagram</p>
-            </div>
-            <div className="px-10 leading-10">
-                <p className="font-bold">Make Money with Us</p>
-                <p className="text-sm leading-7 text-gray-300">Sell on Amazon</p>
-                <p className="text-sm leading-7 text-gray-300">Sell under Amazon Accelerator</p>
-                <p className="text-sm leading-7 text-gray-300">Amazon Global Selling</p>
-                <p className="text-sm leading-7 text-gray-300">Become an Affiliate</p>
-                <p className="text-sm leading-7 text-gray-300">Fulfilment by Amazon</p>
-                <p className="text-sm leading-7 text-gray-300">Advertise Your Products</p>
-                <p className="text-sm leading-7 text-gray-300">Amazon Pay on Merchants</p>
-            </div>
-            <div className="px-10 leading-10">
-                <p className="text-sm leading-7 text-gray-300" className="font-bold">Let Us Help You</p>
-                <p className="text-sm leading-7 text-gray-300">COVID-19 and Amazon</p>
-                <p className="text-sm leading-7 text-gray-300">Your Account</p>
-                <p className="text-sm leading-7 text-gray-300">Returns Centre</p>
-                <p className="text-sm leading-7 text-gray-300">100% Purchase Protection</p>
-                <p className="text-sm leading-7 text-gray-300">Amazon App Download</p>
-                <p className="text-sm leading-7 text-gray-300">Amazon Assistant Download</p>
-                <p className="text-sm leading-7 text-gray-300">Help</p>
+            <div className="grid grid-flow-col grid-cols-4 bg-gray-800 shadow-lg text-white px-20 py-10">
+                <div className="px-10 leading-10">
+                    <p className="font-bold">Get to Know Us</p>
+                    <p className="text-sm leading-7 text-gray-300">About Us</p>
+                    <p className="text-sm leading-7 text-gray-300">Careers</p>
+                    <p className="text-sm leading-7 text-gray-300">Press Releases</p>
+                    <p className="text-sm leading-7 text-gray-300">Amazon Cares</p>
+                    <p className="text-sm leading-7 text-gray-300">Gift a Smile</p>
+                </div>
+                <div className="px-10 leading-10">
+                    <p className="font-bold">Connect with Us</p>
+                    <p className="text-sm leading-7 text-gray-300">Facebook</p>
+                    <p className="text-sm leading-7 text-gray-300">Twitter</p>
+                    <p className="text-sm leading-7 text-gray-300">Instagram</p>
+                </div>
+                <div className="px-10 leading-10">
+                    <p className="font-bold">Make Money with Us</p>
+                    <p className="text-sm leading-7 text-gray-300">Sell on Amazon</p>
+                    <p className="text-sm leading-7 text-gray-300">Sell under Amazon Accelerator</p>
+                    <p className="text-sm leading-7 text-gray-300">Amazon Global Selling</p>
+                    <p className="text-sm leading-7 text-gray-300">Become an Affiliate</p>
+                    <p className="text-sm leading-7 text-gray-300">Fulfilment by Amazon</p>
+                    <p className="text-sm leading-7 text-gray-300">Advertise Your Products</p>
+                    <p className="text-sm leading-7 text-gray-300">Amazon Pay on Merchants</p>
+                </div>
+                <div className="px-10 leading-10">
+                    <p className="text-sm leading-7 text-gray-300" className="font-bold">Let Us Help You</p>
+                    <p className="text-sm leading-7 text-gray-300">COVID-19 and Amazon</p>
+                    <p className="text-sm leading-7 text-gray-300">Your Account</p>
+                    <p className="text-sm leading-7 text-gray-300">Returns Centre</p>
+                    <p className="text-sm leading-7 text-gray-300">100% Purchase Protection</p>
+                    <p className="text-sm leading-7 text-gray-300">Amazon App Download</p>
+                    <p className="text-sm leading-7 text-gray-300">Amazon Assistant Download</p>
+                    <p className="text-sm leading-7 text-gray-300">Help</p>
+                </div>
             </div>
         </div>
     )
--- src/components/Product.js
+++ src/components/Product.js
@@ -23,7 +23,7 @@ function Product({id, title, price, description, category, image}) {
     }
 
     return (
-        <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+        <div className="relative flex flex-col m-5 bg-white z-30 p-10 rounded-md shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
             <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
 
             <Image src={image} height={200} width={200} objectFit="contain" />
