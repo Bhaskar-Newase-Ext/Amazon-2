fe460193fe6439a51cb9675f8d81fb2e59f0540b
Bhaskar-Newase-Ext <62342525+Bhaskar-Newase-Ext@users.noreply.github.com>
2021-08-19 16:47:42 +0530

    bug fixes

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@ module.exports = {
     images: {
         domains: ["links.papareact.com", "fakestoreapi.com"],
     },
-    env:  {
-        STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
-        GOOGLE_ID: process.env.GOOGLE_ID,
-        GOOGLE_SECRET: process.env.GOOGLE_SECRET,
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-        STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-        STRIPE_SIGNING_SECRET: process.env.STRIPE_SIGNING_SECRET,
-        HOST: process.env.HOST
+    env: {
+        stripe_public_key: process.env.STRIPE_PUBLIC_KEY
     }
 }
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -7,7 +7,8 @@ import { selectItems, selectTotal } from "../slices/basketSlice";
 import { useSession } from "next-auth/client";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
-const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY);
+const publicKey = process.env.stripe_public_key.toString();
+const stripePromise = loadStripe(publicKey);
 
 function Checkout() {
     const items = useSelector(selectItems);
