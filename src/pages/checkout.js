9681900f1199043473edeb4663ec314d29578c6c
Bhaskar-Newase-Ext <62342525+Bhaskar-Newase-Ext@users.noreply.github.com>
2021-08-20 09:51:04 +0530

    bug fixed

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -7,9 +7,8 @@ import { selectItems, selectTotal } from "../slices/basketSlice";
 import { useSession } from "next-auth/client";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
-debugger
 const publicKey = process.env.stripe_public_key.toString();
-const stripePromise = loadStripe("pk_test_51JPNiCSF2ouX2yKjLcPu43B2jP0yjNMrlPc9djMmHTkWVZNPdc4VG8t4vbI6lwa4Qtd9n3ZZFYcW7E5wEDzIjpW800MSvVI4Sa");
+const stripePromise = loadStripe(publicKey);
 
 function Checkout() {
     const items = useSelector(selectItems);
